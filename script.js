3ff3b239e2da1d2411435e6081a27d3a24f968c8
zakir <azizizakir449@gmail.com>
2024-04-17 19:42:16 +0500

    my commit

--- script.js
+++ script.js
@@ -35,3 +35,80 @@ gsap.to("#nav", {
     scrub: 1,
   },
 });
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers: true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#about-us img,#about-us-in", {
+  y: 90,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from(".card", {
+  scale: 0.8,
+  // opacity:0,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers:false,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
+});
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
+
+// Thanks itna aage tak aane ke liye lekin pura code utha ke copy paste karne ki jagah khud ek baar banane ka try karna, kuch naya seekhne ko milega!
